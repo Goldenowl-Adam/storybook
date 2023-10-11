e73070990365c01c1b7cc3c122e5984049f1440d
Adam Ho <adam.ho.goldenowl@gmail.com>
2023-10-11 16:12:41 +0700

    [Feature-day4]: update logic code

--- src/stories/Gallery/Gallery.tsx
+++ src/stories/Gallery/Gallery.tsx
@@ -6,69 +6,73 @@ interface GalleryProps {
 }
 
 export const Gallery = ({ images }: GalleryProps) => {
-    const [isOpenGallery, setIsOpenGallery] = useState(false);
-    const [indexSelectedImage, setIndexSelectedImage] = useState(1);
+   const [indexSelectedImage, setIndexSelectedImage] = useState<number>();
+
     return (
         <div className="container-gallery">
             <div className="wrapper">
-                {images.map(({ id, src }) => (
+                {images.map(({ id, src }, index) => (
                     <div
                         key={id}
                         className="wrapper__image"
                         onClick={() => {
-                            setIsOpenGallery(true);
-                            setIndexSelectedImage(id);
+                            setIndexSelectedImage(index);
                         }}
                     >
                         <img
                             className="wrapper__image--item image"
                             src={src}
-                            alt={src}
+                            alt=""
                         />
                     </div>
                 ))}
             </div>
-            {isOpenGallery && <div className="bg-cover"></div>}
+            {indexSelectedImage !== undefined && (
+                <>
+                    <div className="bg-cover" />
+                    <div className="gallery">
+                        {indexSelectedImage !== 0 && (
+                            <span
+                                className="gallery__control"
+                                onClick={() => {
+                                    setIndexSelectedImage(
+                                        indexSelectedImage - 1
+                                    );
+                                }}
+                            >
+                                <i className="gallery__control--left fas fa-chevron-left" />
+                            </span>
+                        )}
 
-            {isOpenGallery && (
-                <div className="gallery">
-                    {indexSelectedImage !== 1 && (
-                        <span
-                            className="gallery__control"
+                        {indexSelectedImage !== images.length - 1 && (
+                            <span
+                                className="gallery__control"
+                                onClick={() => {
+                                    setIndexSelectedImage(
+                                        indexSelectedImage + 1
+                                    );
+                                }}
+                            >
+                                <i className="gallery__control--right fas fa-chevron-right" />
+                            </span>
+                        )}
+
+                        <div className="gallery__image">
+                            <img
+                                className="image"
+                                src={images[indexSelectedImage].src}
+                                alt=""
+                            />
+                        </div>
+
+                        <i
+                            className="fas fa-times gallery__btn-close"
                             onClick={() => {
-                                setIndexSelectedImage(indexSelectedImage - 1);
+                                setIndexSelectedImage(undefined);
                             }}
-                        >
-                            <i className="gallery__control--left fas fa-chevron-left" />
-                        </span>
-                    )}
-
-                    {indexSelectedImage !== images.length && (
-                        <span
-                            className="gallery__control"
-                            onClick={() => {
-                                setIndexSelectedImage(indexSelectedImage + 1);
-                            }}
-                        >
-                            <i className="gallery__control--right fas fa-chevron-right" />
-                        </span>
-                    )}
-
-                    <div className="gallery__image">
-                        <img
-                            className="image"
-                            src={images[indexSelectedImage - 1].src}
-                            alt={images[indexSelectedImage - 1].src}
                         />
                     </div>
-
-                    <i
-                        className="fas fa-times gallery__btn-close"
-                        onClick={() => {
-                            setIsOpenGallery(false);
-                        }}
-                    />
-                </div>
+                </>
             )}
         </div>
     );
